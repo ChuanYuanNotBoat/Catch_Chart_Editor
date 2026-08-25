cfc79b0fad538401e1debe9a0142100dd1e25878
Chuanyuan <124045257+ChuanYuanNotBoat@users.noreply.github.com>
2026-08-25 19:57:06 +0800

    chore: advance development version to 1.11.0

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
     {
         // Set application name and version.
         QCoreApplication::setApplicationName("Malody Catch Chart Editor");
-        QCoreApplication::setApplicationVersion("Beta v1.10.5");
+        QCoreApplication::setApplicationVersion("Beta v1.11.0");
 
         Application app(argc, argv);
 
